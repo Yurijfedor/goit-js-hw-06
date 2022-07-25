60d4db97dd065bad184cd00714d82344fedee203
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-07-25 13:52:02 +0300

    update task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const itemsLiEl = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  return liEl;
+});
+
+ingredientsListEl.append(...itemsLiEl);
